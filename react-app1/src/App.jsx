8b00e427aefcf42e097cc12da3035e843b54bd43
gdj97 <goodeegdj90+7@gmail.com>
2026-05-11 17:39:50 +0900

    20260511-1739

---
+++ react-app1/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'   //사용하지 않음. Hook이라고함.  
+import './App.css'       //사용하지 않음. css 파일 불러옴
+/**
+ * 문제 : 안녕하세요 홍길동 문장을 화면출력하기
+      -  Welcome.jsx 컴포넌트로 만들기
+       - 검정색에 하얀글씨로
+       - 홍길동 이름은 변수로 처리하기
+ */
+function App() {
+}
+
+export default App 
